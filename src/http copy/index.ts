c7ca8011f2dcbfe733c9398c2ce18f8a4183774e
赵政锜 <18331543819@163.com>
2024-10-11 11:13:06 +0800

    fix: 测试lint-staged

---
+++ src/http copy/index.ts
@@ -0,0 +1,28 @@
+import type { AxiosRequestConfig } from "axios";
+
+import requestInstance from "./request";
+
+// 在最后使用封装过的axios导出不同的请求方式
+
+export function get<T = any, U = any>(
+  url: string,
+  params?: U,
+
+  config?: AxiosRequestConfig
+): Promise<T> {
+  return requestInstance({
+    url,
+    method: "GET",
+    params: params,
+    ...config,
+  });
+}
+
+export function post<T = any, U = any>(
+  url: string,
+  data: U,
+
+  config?: AxiosRequestConfig
+): Promise<T> {
+  return requestInstance({ url, method: "POST", data: data, ...config });
+}
